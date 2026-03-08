2a8cf38e3028b0125e410fd0de49c8301373de2f
Thomas Motley <zcbtmot@ucl.ac.uk>
2026-03-08 13:39:43 +0000

    fixed annotations

--- Annotation.R
+++ Annotation.R
@@ -1,6 +1,8 @@
 #Annotation of Data
 data <- main_production_assay
 
+data <- na.omit(data)
+
 #Create matched vs mismatched column 
 data$status <- NA_character_
 data$status[data$genotype %in% c("mM","tT")] <- "matched"
@@ -12,3 +14,15 @@ data$stage <- NA_character_
 data$stage <- ifelse(data$pop %in% 21:26, "f2","fx")
 data$stage <- factor(data$stage, levels = c("fx","f2"))
 
+#Plot Information 
+stars <- ifelse(p_val < 0.001, "***",
+                ifelse(p_val < 0.01, "**",
+                       ifelse(p_val < 0.05, "*", "ns")))
+
+stars2 <- ifelse(p_val2 < 0.001, "***",
+                ifelse(p_val2 < 0.01, "**",
+                       ifelse(p_val2 < 0.05, "*", "ns")))
+
+stars3 <- ifelse(p_val3 < 0.001, "***",
+                 ifelse(p_val3 < 0.01, "**",
+                        ifelse(p_val3 < 0.05, "*", "ns")))
